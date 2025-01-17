cb2d24a18b0adc3c2301c521984246ab91645c60
Peter Jones <944541960@qq.com>
2025-01-17 23:43:10 +0800

    新增 单元测试项目

---
+++ FangJia_UnitTest/UnitTestAppWindow.xaml.cs
@@ -0,0 +1,15 @@
+using Microsoft.UI.Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace FangJia_UnitTest
+{
+    public sealed partial class UnitTestAppWindow : Window
+    {
+        public UnitTestAppWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FangJia_UnitTest/UnitTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+
+namespace FangJia_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, 0);
+        }
+
+        // Use the UITestMethod attribute for tests that need to run on the UI thread.
+        [UITestMethod]
+        public void TestMethod2()
+        {
+            var grid = new Grid();
+            Assert.AreEqual(0, grid.MinWidth);
+        }
+    }
+}
